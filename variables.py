24e848b174c3ff947bddca64a647dda8c514f2f3
Sunny242545 <chakrisundeep007@gmail.com>
2025-11-11 13:32:13 -0500

    Simple commit with input funtion and print usage

---
+++ variables.py
@@ -0,0 +1,8 @@
+# This is a simple python print funtion with deafult string
+s = "Hello World"
+print(s)
+
+
+# This is a simple python print funtion with custom input string
+s = input("Enter a string: ")
+print("Hello "+s)
