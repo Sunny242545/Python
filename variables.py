e9148ac4c8ffd80305a39b80cd8ea72c92a50e82
Sunny242545 <chakrisundeep007@gmail.com>
2025-11-18 23:41:40 -0500

    decorators,generators, iterators

--- variables.py
+++ variables.py
@@ -497,12 +497,43 @@
 # student2 = Student(professor, "Mahi")
 # student2.learn()
 
+# Decorators
 
-import pandas as pd
-class ReadData:
-    def __init__(self, data = None):
-        self.data = pd.read_csv(data)
-    def display(self):
-        print(self.data.head())
-read_data = ReadData("C:\\Users\\chakr\\OneDrive\\Desktop\\data.txt.txt")
-read_data.display()
+# #Iterator Example
+
+# class CountNumber:
+#     def __init__(self, max_val):
+#         self.max_val = max_val
+#         self.count = 0
+#     def __iter__(self):
+#         return self
+#     def __next__(self):
+#         if self.count < self.max_val:
+#             self.count +=1
+#             return self.count
+#         else:
+#             raise StopIteration
+# for i in CountNumber(10):
+#     print(i)
+
+# # generators
+# def CountGenerator(val):
+#     current =0
+#     while current < val:
+#         yield current
+#         current +=1
+# for i in CountGenerator(10):
+#     print(i)
+
+# #Decorator example
+# def decorator(func):
+#     def wrapper():
+#         print("this code executed before job function")
+#         return func()
+#     return wrapper
+
+# @decorator
+# def job():
+#     print("this is my job function")
+
+# job()
